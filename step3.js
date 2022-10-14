6bf82f3dd4ad4dec44b3ca31302081a096ff1e2c
jtompkins415 <john.tompkins415@gmail.com>
2022-10-14 09:31:46 -0700

    create function outputCat and adjust earlier functions

---
+++ step3.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const process = require('process')
+const axios = require('axios')
+
+function outputCat(text, out){
+    if(out){
+        fs.writeFile(text, out, 'utf8', (err) => {
+            if(err){
+                console.log(err);
+                process.exit(1)
+            }
+        });
+    } else {
+        console.log(text);
+    }
+}
+
+function cat(path, out){
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err){
+            console.log("ERROR:", err)
+            process.exit(1)
+        } else {
+           outputCat(data, out)
+        }
+    })
+}
+
+
+async function webCat(url, out){
+    try{
+        const resp = await axios.get(url)
+        outputCat(resp, out)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+let path
+let out
+
+if(process.argv[2] === '--out') {
+    out = process.argv[3]
+    path = process.argv[4]
+} else {
+    path === process.argv[2]
+}
+
+if (path.slice(0, 4) === 'http') {
+    webCat(path, out);
+  } else {
+    cat(path, out);
+  }
+
+
